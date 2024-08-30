4e70618a8cacdc4bb4076ea71b5cdf88b746b648
Kiran <km060360@gmail.com>
2024-08-30 14:38:26 +0000

    fe: confirmGitAuto

--- gitAuto.sh
+++ gitAuto.sh
@@ -3,9 +3,16 @@
 # Prompt for additional commit message input
 read -p "Enter commit message: " COMMIT_MESSAGE
 
+# Check if COMMIT_MESSAGE is empty
+if [ -z "$COMMIT_MESSAGE" ]; then
+  echo "Commit message cannot be empty."
+  exit 1
+fi
+
 # Perform Git commands
 git add .
 git commit -m "fe: ${COMMIT_MESSAGE}"
 git push origin main
 
-echo "Commit fe: ${COMMIT_MESSAGE} pushed to origin main."
+# Confirmation message
+echo "Commit 'fe: ${COMMIT_MESSAGE}' pushed to origin main."
